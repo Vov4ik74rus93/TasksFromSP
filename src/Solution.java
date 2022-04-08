2ff1f418b52082d6d6aeee804afbd7bcd970a1b9
vov4ik74rus93 <vov4ik74rus93@gmail.com>
2022-04-08 21:09:28 +0500

    ...

--- src/Solution.java
+++ src/Solution.java
@@ -4,7 +4,14 @@ import task02Matrix.Matrix;
 public class Solution {
     public static void main(String[] args) {
         //System.out.println(new Int(1000));
-        Matrix a = new Matrix(3, 5);
-
+        int row = 3;
+        int column = 5;
+        Matrix a = new Matrix(row, column);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                a.setElement(i, j, (int) (Math.random()*99));
+            }
+        }
+        System.out.println(a);
     }
 }
